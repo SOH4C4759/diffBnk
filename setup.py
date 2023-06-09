dd37d99d231a2fed5ee7bea09b1fb39b6266705e
冠宇 林 <lgy19971027@163.com>
2023-06-10 02:44:00 +0800

    提交setup.py文件

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='diffErrBnk',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/SOH4C4759/diffBnk',
+    license='',
+    author='Sora',
+    author_email='vx:SOH4C4759',
+    description='用于自动化修复Wwise升级2022版本后，安卓平台bnk构建异常的问题'
+)
