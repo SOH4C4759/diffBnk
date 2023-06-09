8fd9ee834f0af1a59fdb4f83fab82c3d7019969e
冠宇 林 <lgy19971027@163.com>
2023-06-10 03:09:15 +0800

    提交yml文件

--- setup.py
+++ setup.py
@@ -8,6 +8,5 @@ setup(
     url='https://github.com/SOH4C4759/diffBnk',
     license='',
     author='Sora',
-    author_email='vx:SOH4C4759',
     description='用于自动化修复Wwise升级2022版本后，安卓平台bnk构建异常的问题'
 )
